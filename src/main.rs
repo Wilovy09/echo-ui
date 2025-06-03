29809b4407887744993df46ad2a43715d635661c
Emiliano Maldonado Garza <srwilovy09@gmail.com>
2025-06-03 14:22:31 -0600

    🔥Refactor button implementation to use EButton and update button variants

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-use components::normal::CButton;
+use components::buttons::EButton;
 use freya::prelude::*;
-use utils::variants::Variants;
+
+use crate::components::buttons::Variants;
 
 mod components;
-mod utils;
 
 fn main() {
     launch_with_props(app, "Component Library", (500.0, 500.0))
@@ -16,7 +16,8 @@ fn get_theme(preferred_theme: PreferredTheme) -> Theme {
     }
 }
 
-static PRE_ICON: &[u8] = include_bytes!("./assets/icons/fork.svg");
+static PRE_FORK: &[u8] = include_bytes!("./assets/icons/fork.svg");
+static PRE_ANCHOR: &[u8] = include_bytes!("./assets/icons/anchor.svg");
 
 fn app() -> Element {
     let preferred_theme = use_preferred_theme();
@@ -29,7 +30,8 @@ fn app() -> Element {
         }
     });
 
-    let ICON = static_bytes(PRE_ICON);
+    let fork_icon = static_bytes(PRE_FORK);
+    let anchor_icon = static_bytes(PRE_ANCHOR);
 
     rsx!(
         Body {
@@ -38,23 +40,73 @@ fn app() -> Element {
                 height: "fill",
                 main_align: "center",
                 cross_align: "center",
-                direction: "horizontal",
+                direction: "vertical",
                 rect {
-                    CButton {
-                        text: "Default",
-                    }
-                    CButton {
-                        text: "Secondary",
-                        variant: Variants::Secondary,
-                        svg {
-                            svg_data: ICON,
-                            width: "20",
-                            height: "20"
+                    direction: "horizontal",
+                    rect { margin: "10",
+                        EButton {
+                            text: "Default",
                         }
                     }
-                    CButton {
-                        text: "Desctructive",
-                        variant: Variants::Destructive,
+                    rect { margin: "10",
+                        EButton {
+                            text: "Secondary",
+                            variant: Variants::Secondary,
+                            svg {
+                                svg_data: fork_icon,
+                                width: "20",
+                                height: "20"
+                            }
+                        }
+                    }
+                    rect { margin: "10",
+                        EButton {
+                            text: "Desctructive",
+                            variant: Variants::Destructive,
+                        }
+                    }
+                }
+                rect{
+                    direction: "horizontal",
+                    rect { margin: "10",
+                        EButton {
+                            text: "Outline",
+                            variant: Variants::Outline,
+                        }
+                    }
+                    rect { margin: "10",
+                        EButton {
+                            text: "Ghost",
+                            variant: Variants::Ghost,
+                        }
+                    }
+                    rect { margin: "10",
+                        EButton {
+                            variant: Variants::Destructive,
+                            svg {
+                                svg_data: anchor_icon,
+                                width: "20",
+                                height: "20"
+                            }
+                        }
+                    }
+                }
+                rect{
+                    direction: "horizontal",
+                    rect { margin: "10",
+                        EButton {
+                            theme: theme_with!(ButtonTheme {
+                                font_theme: theme_with!(FontTheme {
+                                    color: "#FFFFFF".into()
+                                }),
+                                background: "#123456".into(),
+                                border_fill: "#00000000".into(),
+                                focus_border_fill: "#654321".into(),
+                                hover_background: "#567890".into(),
+                                padding: "10".into(),
+                            }),
+                            text: "Custom theme"
+                        }
                     }
                 }
             }
